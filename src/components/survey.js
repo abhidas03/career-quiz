9e1166dd376d6835558c74388093eea38fb72245
Abhi <abhijit.das3002@gmail.com>
2024-11-12 19:49:09 -0500

    Remove boilerplate, add in survey

---
+++ src/components/survey.js
@@ -0,0 +1,92 @@
+export const json = {
+    "title": "Survey",
+    "description": "A survey to help you find your career path!",
+    "logoPosition": "right",
+    "pages": [
+      {
+        "name": "page1",
+        "title": "Page 1",
+        "description": "Answer these questions to reflect how you ACTUALLY feel, now how you WANT to feel!",
+        "elements": [
+          {
+            "type": "rating",
+            "name": "question1",
+            "title": "I feel most energized when I’m creating something new from scratch.\n",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question3",
+            "title": "I seek out new experiences and love exploring uncharted paths.\n",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question2",
+            "title": "Helping others grow and feel supported is highly fulfilling for me.",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question4",
+            "title": "I feel most energized when I am seeking to solve a big problem or answer a meaningful question.",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question5",
+            "title": "When I am helping others learn and grow, it feels like I am doing the work I am meant to do.",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question6",
+            "title": "I find purpose in crafting things with care and precision.",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question7",
+            "title": "Supporting others emotionally and understanding their feelings is something I naturally enjoy.",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question8",
+            "title": "I love sharing stories or ideas in a way that captivates people.",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question9",
+            "title": "I’m drawn to uncovering insights and patterns in data or information.\n",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question10",
+            "title": "My ideal work involves pushing the boundaries of human knowledge ",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question11",
+            "title": "I mostly enjoy work that requires use of physical strength and effort, or working with my hands.",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question12",
+            "title": "For as long as I can remember, I've loved organizing things",
+            "rateType": "smileys"
+          },
+          {
+            "type": "rating",
+            "name": "question13",
+            "title": "I feel the most passionate about work that is working to bring people together and igniting change.",
+            "rateType": "smileys"
+          }
+        ]
+      }
+    ]
+  }
